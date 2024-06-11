67e95d230a3c7b8421d09cb5b670b665b78544ca
davidpacco <david.j.pacco@gmail.com>
2024-06-11 17:45:00 -0500

    Add exercise 5.7 - blog list frontend step 7

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,7 +1,27 @@
+import { useState } from "react"
+
 export function Blog({ blog }) {
+  const [isDetailOpen, setIsDetailOpen] = useState(false)
+  const blogStyle = {
+    padding: '10px 8px',
+    border: 'solid',
+    borderRadius: 8,
+    borderWidth: 1,
+    marginBottom: 8
+  }
+
+  const toggleDetail = () => setIsDetailOpen(!isDetailOpen)
+
   return (
-    <div>
-      {blog.title} - {blog.author}
+    <div style={blogStyle}>
+      {blog.title} - {blog.author} <button onClick={toggleDetail}>{isDetailOpen ? 'Hide' : 'View'}</button>
+      {isDetailOpen &&
+        <div>
+          <p>{blog.url}</p>
+          <p>Likes {blog.likes} <button>Like</button></p>
+          <p>{blog.user.name}</p>
+        </div>
+      }
     </div>
   )
 }
