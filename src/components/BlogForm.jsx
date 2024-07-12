3e2a151adecc87c20cc9f555dbd68271cb1f2ab1
davidpacco <david.j.pacco@gmail.com>
2024-07-12 18:02:47 -0500

    Remove blogFormReducer

--- src/components/BlogForm.jsx
+++ src/components/BlogForm.jsx
@@ -1,22 +1,21 @@
+import { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { createBlog } from '../reducers/blogsReducer'
-import {
-  setAuthor,
-  setTitle,
-  setUrl,
-  resetBlogForm,
-} from '../reducers/blogFormReducer'
 
 export function BlogForm({ blogFormRef }) {
   const dispatch = useDispatch()
   const user = useSelector(({ user }) => user)
-  const { title, author, url } = useSelector(({ blogForm }) => blogForm)
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [url, setUrl] = useState('')
 
   const addBlog = async e => {
     e.preventDefault()
     dispatch(createBlog({ title, author, url }, user))
-    dispatch(resetBlogForm())
     blogFormRef.current.toggleVisibility()
+    setTitle('')
+    setAuthor('')
+    setUrl('')
   }
 
   if (user !== null)
@@ -32,7 +31,7 @@ export function BlogForm({ blogFormRef }) {
                 name="title"
                 type="text"
                 value={title}
-                onChange={e => dispatch(setTitle(e.target.value))}
+                onChange={e => setTitle(e.target.value)}
                 placeholder="Enter the blog title"
                 data-testid="title"
               />
@@ -46,7 +45,7 @@ export function BlogForm({ blogFormRef }) {
                 name="title"
                 type="text"
                 value={author}
-                onChange={e => dispatch(setAuthor(e.target.value))}
+                onChange={e => setAuthor(e.target.value)}
                 placeholder="Enter the author's name"
                 data-testid="author"
               />
@@ -60,7 +59,7 @@ export function BlogForm({ blogFormRef }) {
                 name="url"
                 type="text"
                 value={url}
-                onChange={e => dispatch(setUrl(e.target.value))}
+                onChange={e => setUrl(e.target.value)}
                 placeholder="Enter the URL"
                 data-testid="url"
               />
--- src/store.js
+++ src/store.js
@@ -1,14 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 import notificationReducer from './reducers/notificationReducer'
 import blogsReducer from './reducers/blogsReducer'
-import blogFormReducer from './reducers/blogFormReducer'
 import userReducer from './reducers/userReducer'
 
 const store = configureStore({
   reducer: {
     notification: notificationReducer,
     blogs: blogsReducer,
-    blogForm: blogFormReducer,
     user: userReducer,
   },
 })
